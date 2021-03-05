fix normalization scheme

melody_utils.py
@@ -175,9 +175,9 @@ class MelodyLoader:
     
     def get_overlapped_contours(self, path, win_size=2000, hop_size=500, min_ratio=0.5):
         contour = load_melody(path)
-        melody_form = pitch_array_to_formatted(np.asarray(contour))
+        melody_form = scale_to_midi(np.asarray(contour))
+        melody_form = pitch_array_to_formatted(melody_form)
         # melody_ds = downsample_contour(contour)
-        melody_form = scale_to_midi(melody_form)
         slice_pos = list(range(0, melody_form.shape[0] - win_size, hop_size))
         slice_pos.append(melody_form.shape[0] - win_size)
         array_overlapped = np.asarray([melody_form[i:i+win_size] for i in slice_pos])
@@ -424,8 +424,11 @@ def quantizing_hz(contour, to_midi=False, quantization=True):
     return [quantize_or_return_zero(x) for x in contour]
 
 def scale_to_midi(contour):
-    contour[contour[:,1]==1,0] = np.log2(contour[contour[:,1]==1,0] / 440) * 12 + 69
-    return contour
+    # contour[contour[:,1]==1,0] = np.log2(contour[contour[:,1]==1,0] / 440) * 12 + 69
+    is_pitch = np.nonzero(contour)
+    contour_array = np.copy(contour)
+    contour_array[is_pitch] = np.log2(contour_array[is_pitch] / 440) * 12 + 69
+    return contour_array
 
 def load_melody(path):
     with open(path, "r") as f:

model.py
@@ -177,9 +177,9 @@ class ContextAttention(nn.Module):
 
 
 
-class ConvNorm(nn.Module):
+class ConvNormForRes(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size):
-        super(ConvNorm, self).__init__()
+        super(ConvNormForRes, self).__init__()
         self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, padding=(kernel_size-1) // 2, bias=False)
         self.bn = nn.BatchNorm2d(out_channels, eps=0.001, momentum=0.01)
         self.activation = nn.LeakyReLU(0.01)
@@ -197,10 +197,10 @@ class ConvNorm(nn.Module):
 class ResNet_Block(nn.Module):
     def __init__(self, num_input_ch, num_channels):
         super(ResNet_Block, self).__init__()
-        self.conv1 = ConvNorm(num_input_ch, num_channels, 1)
-        self.conv2 = ConvNorm(num_channels, num_channels, 3)
-        self.conv3 = ConvNorm(num_channels, num_channels, 3)
-        self.conv4 = ConvNorm(num_channels, num_channels, 1)
+        self.conv1 = ConvNormForRes(num_input_ch, num_channels, 1)
+        self.conv2 = ConvNormForRes(num_channels, num_channels, 3)
+        self.conv3 = ConvNormForRes(num_channels, num_channels, 3)
+        self.conv4 = ConvNormForRes(num_channels, num_channels, 1)
         
     def cal_conv(self,x):
         return self.conv4(self.conv3(self.conv2(self.conv1(x))))