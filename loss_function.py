fix loss calculation

loss_function.py
@@ -10,7 +10,7 @@ def cross_entropy(pred, target):
 
 
 class SiameseLoss:
-    def __init__(self, margin=0.4, use_euclid=False):
+    def __init__(self, margin=0.4, use_euclid=False, use_elementwise=False):
         if use_euclid:
             # self.similiarity_fn = torch.dist
             def euclidean_sim(x,y):
@@ -21,6 +21,11 @@ class SiameseLoss:
         else:
             self.similiarity_fn = torch.nn.CosineSimilarity(-1, eps=1e-6)
         self.margin = margin
+
+        if use_elementwise:
+            self.max_hinge_loss = self.elementwise_max_hinge_loss
+        else:
+            self.max_hinge_loss = self.mean_max_hinge_loss
 
     def cal_similarity(self, anchor, pos, neg):
         # if anchor.shape[1] != neg.shape[1]:
@@ -41,11 +46,19 @@ class SiameseLoss:
         pos_similarity = self.similiarity_fn(anchor, pos)
         return pos_similarity, neg_similarity
 
-    def max_hinge_loss(self, anchor, pos, neg, return_item=False):
+    def elementwise_max_hinge_loss(self, anchor, pos, neg, return_item=False):
         pos_similarity, neg_similarity = self.cal_similarity(anchor, pos, neg)
-        # if pos_similarity.shape == neg_similarity:
-        #     return torch.mean(torch.max(torch.zeros_like(pos_similarity), self.margin - pos_similarity + neg_similarity))
-        # else:
+        loss_sum = torch.zeros_like(pos_similarity)
+        for i in range(neg_similarity.shape[1]):
+            loss_sum += torch.max(torch.zeros_like(pos_similarity), self.margin - pos_similarity + neg_similarity[:,i:i+1])
+        loss_sum /= i+1
+        if return_item:
+            return loss_sum
+        else:
+            return torch.mean(loss_sum)
+
+    def mean_max_hinge_loss(self, anchor, pos, neg, return_item=False):
+        pos_similarity, neg_similarity = self.cal_similarity(anchor, pos, neg)
         pos_similarity = torch.mean(pos_similarity, axis=-1)
         neg_similarity = torch.mean(neg_similarity, axis=-1)
         loss = torch.max(torch.zeros_like(pos_similarity), self.margin - pos_similarity + neg_similarity)
@@ -53,5 +66,6 @@ class SiameseLoss:
             return loss
         else:
             return torch.mean(loss)
+
     def __call__(self, anchor, pos, neg, return_item=False):
         return self.max_hinge_loss(anchor, pos, neg, return_item)

model.py
@@ -282,10 +282,10 @@ class CombinedModel(nn.Module):
             audio_input, contour_pos_n_neg = input
             num_batch = audio_input.shape[0] // (num_neg+1)
             _, voice_hidden = self.singing_voice_estimator(audio_input)
-            voice_hidden_batch = voice_hidden.reshape(num_batch, -1, voice_hidden.shape[-1])
+            # voice_hidden_batch = voice_hidden.reshape(num_batch, -1, voice_hidden.shape[-1])
             # voice_hidden_batch = torch.nn.functional.max_pool1d(voice_hidden_batch.permute(0,2,1), kernel_size=10).permute(0,2,1)
 
-            audio_embedding = self.audio_encoder(voice_hidden_batch)
+            audio_embedding = self.audio_encoder(voice_hidden)
             audio_embedding = audio_embedding.view(num_batch, -1, audio_embedding.shape[-1])
             contour_embedding = self.contour_encoder(contour_pos_n_neg)
             contour_embedding = contour_embedding.view(num_batch, -1 , contour_embedding.shape[-1])
@@ -295,11 +295,10 @@ class CombinedModel(nn.Module):
             return self.contour_encoder(input)
         else:
             audio_input = input
-            num_batch = audio_input.shape[0] // (num_neg+1)
             _, voice_hidden = self.singing_voice_estimator(audio_input)
-            voice_hidden_batch = voice_hidden.reshape(num_batch, -1, voice_hidden.shape[-1])
+            # voice_hidden_batch = voice_hidden.reshape(num_batch, -1, voice_hidden.shape[-1])
             # voice_hidden_batch = torch.nn.functional.max_pool1d(voice_hidden_batch.permute(0,2,1), kernel_size=10).permute(0,2,1)
-            audio_embedding = self.audio_encoder(voice_hidden_batch)
+            audio_embedding = self.audio_encoder(voice_hidden)
             return audio_embedding
         
     def siamese(self, audio_anchor, contour_pos_n_neg, num_batch, num_pos):