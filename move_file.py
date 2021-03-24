e8075e1932936ec38d41d80a18e0d6a5d46a4f82
Dasaem Jeong <dasaem.jeong@sktbrain.com>
2021-03-24 18:46:27 +0900

    fix error in dataparallel

--- move_file.py
+++ move_file.py
@@ -1,14 +1,26 @@
 from pathlib import Path
 import shutil
+import os
+import tqdm
+# pitch_dir = Path('/home/svcapp/userdata/flo_pitch/')
+# pitch_list = pitch_dir.rglob("*.txt")
+# target_dir = '/home/svcapp/t2meta/flo_new_music/music_100k/'
 
-pitch_dir = Path('/home/svcapp/userdata/flo_pitch/')
-pitch_list = pitch_dir.rglob("*.txt")
+# for pitch in pitch_list:
+#     rel_path = '/'.join(str(pitch.parent).split('/')[-2:]) + '/'
+#     if 'qbh' in rel_path:
+#         rel_path = 'qbh/'
+#     target_path = target_dir + rel_path + pitch.name
+#     # print(pitch, target_path)
+#     shutil.copyfile(pitch, target_path)
+
+
+audio_dir = Path('/home/svcapp/t2meta/flo_new_music/music_120k/')
+pitch_list = list(audio_dir.rglob("*.m4a")) + list(audio_dir.rglob("*.aac")) + list(audio_dir.rglob("*.txt"))
 target_dir = '/home/svcapp/t2meta/flo_new_music/music_100k/'
 
-for pitch in pitch_list:
+for pitch in tqdm.tqdm(pitch_list):
     rel_path = '/'.join(str(pitch.parent).split('/')[-2:]) + '/'
-    if 'qbh' in rel_path:
-        rel_path = 'qbh/'
     target_path = target_dir + rel_path + pitch.name
-    # print(pitch, target_path)
-    shutil.copyfile(pitch, target_path)
+    os.makedirs(os.path.dirname(target_path), exist_ok=True)
+    shutil.move(pitch, target_path)
